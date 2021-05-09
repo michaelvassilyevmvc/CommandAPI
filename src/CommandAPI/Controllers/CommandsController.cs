1a338ef8b5b7dcd465d1ab35a4350a0dc6063304
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-05-09 23:02:06 +0600

    Added Model and Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,3 +1,5 @@
+using CommandAPI.Data;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
@@ -7,10 +9,26 @@ namespace CommandAPI.Controllers
   [ApiController]
   public class CommandsController:ControllerBase
   {
-    [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    private readonly ICommandAPIRepo _repostiory;
+
+    public CommandsController(ICommandAPIRepo repostiory)
     {
-      return new string[]{"this","is","hard","coded"};
+      this._repostiory = repostiory;
+    }
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> Get()
+    {
+      var commandItems = _repostiory.GetAllCommands();
+      return Ok(commandItems);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id){
+      var commandItem = _repostiory.GetCommandById(id);
+      if(commandItem == null){
+        return NotFound();
+      }
+      return Ok(commandItem);
     }
   }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,7 @@ namespace CommandAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
